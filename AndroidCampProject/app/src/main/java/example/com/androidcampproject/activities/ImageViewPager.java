4e1b15ffcf6af7775b243ab19be58e898e3e9583
Ihor Demedyuk <ihor.demedyuk@skelia.com.ua>
2015-09-14 19:07:50 +0300

    ViewPager fix

--- AndroidCampProject/app/src/main/java/example/com/androidcampproject/activities/ImageViewPager.java
+++ AndroidCampProject/app/src/main/java/example/com/androidcampproject/activities/ImageViewPager.java
@@ -14,13 +14,13 @@ import java.util.List;
 import example.com.androidcampproject.R;
 import example.com.androidcampproject.adapters.ImagePagerAdapter;
 import example.com.androidcampproject.adapters.PhotoGridAdapter;
+import example.com.androidcampproject.objects.Photo;
 
 /**
  * Created by Esmond on 13.09.2015.
  */
 public class ImageViewPager extends Activity {
     private int position;
-    private int mNum;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -30,16 +30,9 @@ public class ImageViewPager extends Activity {
         setContentView(R.layout.view_pager);
         position = getIntent().getIntExtra("position", 0);
 
-        PhotoGridAdapter photoAdapter = new PhotoGridAdapter(PhotoGridAdapter.photos);
-        List<ImageView> images = new ArrayList<ImageView>();
-
-        for (int i = 0; i < photoAdapter.getItemCount(); i++){
-            ImageView imageView = new ImageView(this);
-            Glide.with(this).load(photoAdapter.photos.indexOf(i)).into(imageView);
-            images.add(imageView);
-        }
-
-        ImagePagerAdapter pagerAdapter = new ImagePagerAdapter(images);
+        List<Photo> photos = PhotoGridAdapter.photos;
+        ImagePagerAdapter pagerAdapter = new ImagePagerAdapter(photos);
+        
         ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
         viewPager.setAdapter(pagerAdapter);
         viewPager.setCurrentItem(position);
--- AndroidCampProject/app/src/main/java/example/com/androidcampproject/adapters/ImagePagerAdapter.java
+++ AndroidCampProject/app/src/main/java/example/com/androidcampproject/adapters/ImagePagerAdapter.java
@@ -1,38 +1,46 @@
 package example.com.androidcampproject.adapters;
 
+import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
+
+import example.com.androidcampproject.R;
+import example.com.androidcampproject.objects.Photo;
 
 /**
  * Created by Esmond on 13.09.2015.
  */
 public class ImagePagerAdapter extends PagerAdapter {
 
-    private List<ImageView> images;
+    private List<Photo> photos;
 
-    public ImagePagerAdapter(List<ImageView> images) {
-        this.images = images;
+    public ImagePagerAdapter(List<Photo> photos) {
+        this.photos = photos;
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        ImageView imageView = images.get(position);
+        LayoutInflater li = (LayoutInflater) container.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ImageView imageView = (ImageView) li.inflate(R.layout.view_pager_image, null);
+        Glide.with(container.getContext()).load(photos.get(position).getSrc_big()).into(imageView);
         container.addView(imageView);
         return imageView;
     }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        container.removeView(images.get(position));
     }
 
     @Override
     public int getCount() {
-        return images.size();
+        return photos.size();
     }
 
     @Override
